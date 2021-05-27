f50c6b1f8952f8f6da8e5a13155b3af0b0a322a6
piuserre <giuseppe.pierri@yahoo.com>
2021-05-27 19:47:23 +0200

    create a new form

--- lib/config/routing/routing.dart
+++ lib/config/routing/routing.dart
@@ -1,3 +1,4 @@
+import 'package:boilerplate/app/form/form_page.dart';
 import 'package:flutter/cupertino.dart';
 
 import '../../app/homepage/homepage.dart';
@@ -5,5 +6,6 @@ import '../login/login_page.dart';
 
 var routesConfig = <String, WidgetBuilder>{
   '/home': (context) => HomePage(title: 'Flutter Demo Home Page'),
-  '/login': (context) => LoginPage()
+  '/login': (context) => LoginPage(),
+  '/createForm': (context) => FormPage()
 };
